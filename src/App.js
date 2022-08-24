9f852d756129a93b6f28139dad4c19e7dc7aad46
zuhrilgtg08 <zuhrilfahrizal08gtg@gmail.com>
2022-08-24 21:38:38 +0700

    develop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useContext, useEffect} from 'react';
+import {useCallback} from 'react';
+const URL = "http://openlibrary.org/search.json?title=";
+const AppContext = React.createContext();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppProvider = ({children}) => {
+    const [search, setSearch] = useState("Atomic Habits");
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [resultTitle, setResultTitle] = useState("");
+
+    const fetchBooks = useCallback(async() => {
+        setLoading(true);
+
+        try {
+            const response = await fetch(`${URL}${search}`);
+            const data = await response.json();
+            const {result} = data;
+
+            if(result) {
+                const newBooks = result.slice(0, 20).map((book) => {
+                    const {key, author_name, cover_id, edition_count, publish_year, title} = book;
+
+                    return {
+                        id: key,
+                        author: author_name,
+                        cover_id: cover_id,
+                        edition_count: edition_count,
+                        first_publish_year: publish_year,
+                        title: title
+                    }
+                });
+
+                setBooks(newBooks);
+
+                if(newBooks.length > 1) {
+                    setResultTitle("Your book were search for has been found");
+                } else {
+                    setResultTitle("Sory, book is NOT FOUND!");
+                }
+            } else {
+                setBooks([]);
+                setResultTitle("Sory, book is NOT FOUND!");
+            }
+            setLoading(false);
+        } catch(error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }, [search]);
+
+    useEffect(() => {
+        fetchBooks();
+    }, [search, fetchBooks]);
+
+    return (
+        <AppContext.Provider value = {{ 
+            loading, books, setSearch, resultTitle, setResultTitle
+        }}>
+            {children}
+        </AppContext.Provider>
+    );
 }
 
-export default App;
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
+
+export {AppContext, AppProvider}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { ReactDOM } from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {AppProvider} from './App.js';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import BookList from './components/BookList/BookList';
+import DetailsBook from './components/DetailsBook/DetailsBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <AppProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element = {<Home/>}>
+                    <Route path='about' element = {<About/>} />
+                    <Route path='book' element = {<BookList/>} />
+                    <Route path='/book/:id' element = {<DetailsBook/>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </AppProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
